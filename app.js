729039e4f8b017498633bb0c6b5dc9a4ffd64d23
yje9802 <jidak1165@gmail.com>
2022-08-07 02:23:49 +0900

    Add additional functions
    직선 그리기 및 직선 그리기 설명 div, 자동 채워주기, 컬러 커스텀 프리셋, 캔버스 초기화 버튼 클릭시 경고 모달창 구현

--- app.js
+++ app.js
@@ -7,14 +7,19 @@ const colorPickerResult = document.querySelector(".current-color");
 const colorOptions = Array.from(
 	document.getElementsByClassName("color-option")
 );
+const customColors = Array.from(document.querySelectorAll(".custom_preset"));
 // mode and etc btns
 const fillBtn = document.querySelector("#fill");
 const lineBtn = document.querySelector("#line");
 const straightBtn = document.querySelector("#straight");
+const autoFillBtn = document.querySelector("#auto-fill");
 const clearBtn = document.querySelector("#reset-canvas");
 const eraseBtn = document.querySelector("#eraser");
 const rectBtn = document.querySelector("#rectangle");
 const circleBtn = document.querySelector("#circle");
+
+const straightBtnInstruction = document.querySelector(".mode-btns-instruction");
+const clearModal = document.querySelector(".clear-modal");
 
 // 하이퍼파라미터
 const CANVAS_WIDTH = 600;
@@ -34,6 +39,9 @@ let currColor = "black";
 let bgColor = "white";
 let currMode = document.querySelector(".mode-selected");
 let selectedColor = document.querySelector(".color-selected");
+// 직선 그리기 조건을 위한 변수
+let firstX = null;
+let firstY = null;
 
 // 캔버스 위에서 마우스가 움직이면
 function onMove(event) {
@@ -50,11 +58,27 @@ function startPainting() {
 }
 function cancelPainting() {
 	isPainting = false;
+	// 면적 자동 채우기 모드 선택시
+	if (isAutoFilling) {
+		ctx.fill();
+	}
+	ctx.beginPath();
 }
 function onCanvasClick(event) {
 	if (isFilling) {
 		bgColor = currColor;
 		ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+	} else if (isStraight) {
+		if (firstX === null && firstY === null) {
+			firstX = event.offsetX;
+			firstY = event.offsetY;
+		} else if (firstX !== null && firstY !== null) {
+			ctx.moveTo(firstX, firstY);
+			ctx.lineTo(event.offsetX, event.offsetY);
+			ctx.stroke();
+			firstX = event.offsetX;
+			firstY = event.offsetY;
+		}
 	}
 }
 
@@ -86,10 +110,33 @@ function onColorClick(event) {
 	selectedColor.classList.remove("color-selected");
 	selectedColor = event.target;
 }
+// custom preset 관련
+function onCustomClick(event) {
+	if (event.target.classList.contains("preset-selected")) {
+		currColor = event.target.dataset.color;
+		ctx.strokeStyle = currColor;
+		ctx.fillStyle = currColor;
+		// 색상 선택기 value도 바꿔줌
+		color.value = currColor;
+		colorPickerResult.innerText = currColor;
+	} else {
+		event.target.style.backgroundColor = currColor;
+		event.target.dataset.color = currColor;
+		event.target.classList.add("preset-selected");
+	}
+}
+
+// 직선 그리기 모드에 대한 설명을 가리는 함수
+function hideInstruction() {
+	straightBtnInstruction.classList.add("hidden");
+}
 
 // 채우기 모드 버튼 선택시
 function onFillClick() {
 	isFilling = true;
+	isStraight = false;
+	isAutoFilling = false;
+	hideInstruction();
 	fillBtn.classList.add("mode-selected");
 	currMode.classList.remove("mode-selected");
 	currMode = fillBtn;
@@ -98,29 +145,75 @@ function onLineClick() {
 	isFilling = false;
 	isStraight = false;
 	isAutoFilling = false;
+	hideInstruction();
 	lineBtn.classList.add("mode-selected");
 	currMode.classList.remove("mode-selected");
 	currMode = lineBtn;
 }
-
-function onClearClick() {
+// 직선 그리기 모드 버튼 선택시
+function onStraightClick() {
+	// 직선 그리기 버튼을 이미 클릭한 상태에서 또 누르면 직선 그리는 starting point (firstX, firstY) 초기화
+	if (straightBtn.classList.contains("mode-selected")) {
+		firstX = null;
+		firstY = null;
+		console.log(firstX, firstY);
+	} else {
+		isStraight = true;
+		isFilling = false;
+		isAutoFilling = false;
+		straightBtn.classList.add("mode-selected");
+		currMode.classList.remove("mode-selected");
+		currMode = straightBtn;
+	}
+	straightBtnInstruction.classList.remove("hidden");
+}
+// 면적 자동 채우기 모드 버튼 클릭시
+function onAutoFillClick() {
+	isAutoFilling = true;
+	isFilling = false;
+	isStraight = false;
+	hideInstruction();
+	autoFillBtn.classList.add("mode-selected");
+	currMode.classList.remove("mode-selected");
+	currMode = autoFillBtn;
+}
+function clearCanvas() {
 	isFilling = false;
 	isStraight = false;
 	isAutoFilling = false;
+	hideInstruction();
 
 	ctx.fillStyle = "white";
 	ctx.fillRect(0, 0, 800, 800);
-	ctx.lineWidth = 5;
-	lineWidth.value = "5";
+	ctx.lineWidth = 10;
+	lineWidth.value = "10";
 
-	clearBtn.classList.add("mode-selected");
-	currMode.classList.remove("mode-selected");
-	currMode = clearBtn;
+	// 캔버스 초기화 후 기존 디폴트 세팅으로 돌아가야 하기에
+	// 선 그리기 버튼이 선택되어 있어야 함
+	if (lineBtn.classList.contains("mode-selected")) {
+		currMode = lineBtn;
+	} else {
+		lineBtn.classList.add("mode-selected");
+		currMode.classList.remove("mode-selected");
+		currMode = lineBtn;
+	}
+	// 캔버스 초기화 작업 후 모달창 닫아줌
+	clearModal.classList.add("hidden");
+}
+function onClearClick() {
+	clearModal.classList.remove("hidden");
+	const clearYes = clearModal.querySelector(".clear-yes");
+	const clearNo = clearModal.querySelector(".clear-no");
+	clearYes.addEventListener("click", clearCanvas);
+	clearNo.addEventListener("click", () => {
+		clearModal.classList.add("hidden");
+	});
 }
 function onEraseClick() {
 	isFilling = false;
 	isStraight = false;
 	isAutoFilling = false;
+	hideInstruction();
 
 	ctx.strokeStyle = bgColor;
 
@@ -139,9 +232,14 @@ canvas.addEventListener("click", onCanvasClick);
 lineWidth.addEventListener("change", onLineWidthChanged);
 color.addEventListener("change", onColorChange);
 colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+customColors.forEach((custom) =>
+	custom.addEventListener("click", onCustomClick)
+);
 
 fillBtn.addEventListener("click", onFillClick);
 lineBtn.addEventListener("click", onLineClick);
+straightBtn.addEventListener("click", onStraightClick);
+autoFillBtn.addEventListener("click", onAutoFillClick);
 // 캔버스 초기화
 clearBtn.addEventListener("click", onClearClick);
 // 지우개 버튼 활성화
